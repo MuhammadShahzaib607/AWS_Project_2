98ddccd37b95c3dc61d4ca878e1626bf27708d00
MuhammadShahzaib607 <muhammadshahzaib6077@gmail.com>
2026-07-13 10:03:29 +0000

    frontend deploy

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,12 +1,12 @@
 const awsConfig = {
-    region: 'us-west-2',
+    region: 'us-east-1', 
     cognito: {
+        region: 'ap-south-1',
         userPoolId: 'ap-south-1_CX3r85Vdg',
-        clientId: '47v17eadiqcd6bp7jk50n91jqi',
-        domain: 'REPLACE_WITH_COGNITO_DOMAIN'
+        clientId: '47v17eadiqcd6bp7jk50n91jqi'
     },
     apiGateway: {
-        invokeUrl: 'https://REPLACE_WITH_API_ID.execute-api.us-west-2.amazonaws.com/dev'
+        invokeUrl: 'https://vhf9htxanf.execute-api.us-east-1.amazonaws.com/dev'
     },
     dynamodb: {
         tableName: 'modern-todos'
